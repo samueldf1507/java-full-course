08e66b9a2972b6043888686d1c2ce92d4e0ca254
Samuel De Francesco <defrancescodev@gmail.com>
2025-08-30 12:55:13 -0300

    Create StringMain01.java

---
+++ src/academy/devdojo/maratonajava/Qstring/test/StringMain01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.Qstring.test;
+
+public class StringMain01 {
+    public static void main(String[] args) {
+        /*
+        As variáveis name1 e name2 estão fazendo referência pro mesmo objeto em memória
+         */
+        String name = "William"; //String constant pool
+        String name2 = "William";
+        name = name.concat(" Suane");
+        System.out.println(name);
+        System.out.println(name == name2);
+        String name3 = new String("William"); //1 Variavel de referência, 2 objeto do tipo String, 3 uma String no pool de Strings
+        System.out.println(name2 == name3.intern());
+        System.out.println(name3.intern());
+    }
+}
